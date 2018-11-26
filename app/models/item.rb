604ad7f6e522b8a21bd1b9bafcba326b524ddc55
kirimari <ait.mus06@gmail.com>
2018-11-26 14:14:33 +0100

    Create Model Item with validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates: title, presence: true
+  validates: description, presence: true
+  validates: image_url, presence: true
+  validates: price, presence: true
 end
