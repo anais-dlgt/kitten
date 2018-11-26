dd362a6174a4d9b64169e16168236af6b042325a
kirimari <ait.mus06@gmail.com>
2018-11-26 16:06:55 +0100

    Devise configuration : Devise Finished

--- db/schema.rb
+++ db/schema.rb
@@ -27,17 +27,15 @@ ActiveRecord::Schema.define(version: 2018_11_26_134221) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
