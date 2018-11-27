445d56f989cf2ef3ec214f900f8ea89e3e22f85f
TheLondonWhale <thomas.perus@espeme.com>
2018-11-27 12:19:07 +0100

    mise en place panier simple et ajout lien User/Cart sur migration

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   has_many :line_items, dependent: :nullify
+  has_many :orders
   validates :title, presence: true
   validates :description, presence: true
   validates :image_url, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
+  has_one :cart
+
   attr_accessor :login
+
   before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
   validates :password, presence: true, length: { minimum: 6 }
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+
   validates :username, presence: true, length: { maximum: 50}, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -21,4 +28,5 @@ class User < ApplicationRecord
       where(conditions.to_hash).first
     end
   end
+
 end
