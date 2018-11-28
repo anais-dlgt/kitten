e355d8f716704aee0bf393c021be50f3fc9d0877
kirimari <ait.mus06@gmail.com>
2018-11-28 11:25:48 +0100

    Modif sur schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_211335) do
+ActiveRecord::Schema.define(version: 2018_11_27_224612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "digits"
+    t.integer "month"
+    t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
