e38ac1954301f034e31df74ccf778ba6aa99f167
kirimari <ait.mus06@gmail.com>
2018-11-29 14:32:14 +0100

    Toujours sur  ActiveStorage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  has_one_attached :picture
   has_many :line_items, dependent: :nullify
+
 
   validates :title, presence: true
   validates :description, presence: true
@@ -11,5 +11,5 @@ class Item < ApplicationRecord
     (price*100).to_i
   end
 
-  @item.attach(params[:picture])
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_one :cart
-  has_one_attached :avatar
-  after_create :send_welcome_email
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
