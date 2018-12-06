1538aa8e66cb947196c5cc5ee6b05b97cd0fa118
Anaïs <anais.diologeant@hotmail.fr>
2018-12-06 09:13:35 +0100

    modif application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email,
       :password, :password_confirmation)}
 
-      devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:username, :email,
-        :password, :password_confirmation,:remember_me)}
+    devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:username, :email,
+      :password, :password_confirmation,:remember_me)}
   end
 
 
